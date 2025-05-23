06c7ea01fa96e415266d3d235f8d1da1399fcdf5
Luciano Abreu <lucianocaboculo@gmail.com>
2025-05-23 11:00:38 -0300

    refactor: change Header and CoreConcept to separeted files also create Header.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,7 @@
-import reactImg from "./assets/react-core-concepts.png";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 import { CORE_CONCEPTS } from "./data.js";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core", "Essential"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[getRandomInt(3)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
